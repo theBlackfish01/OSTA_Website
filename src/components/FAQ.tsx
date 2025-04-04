d71df81244ddbf535db8f9a0ac94040b4c270a5a
theBlackfish01 <wadood2003@gmail.com>
2025-04-05 01:12:24 +0500

    almost done, working website commit

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -4,7 +4,6 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
